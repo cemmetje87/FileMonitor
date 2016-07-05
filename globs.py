cd90be129570570eae2ef551a95880aeca924788
Cem Ozugur <cemmetje@gmail.com>
2016-07-05 21:52:19 +0200

    Create globs.py

---
+++ globs.py
@@ -0,0 +1,23 @@
+import toml
+import datetime
+import os
+import sys
+import envelopes
+import arrow
+
+
+#Prompt user to load config file or to select th folder/file location manually
+print("""
+------------------------------------------------------
+|  File Monitoring Tool 0.0.1                        |
+|  CreativeMinds                                     |
+|  Developed by Cem Ozugur                           |
+------------------------------------------------------
+""")
+#load_config_query = input("Would you like to load config file?: {0}, {1}".format("(Y)es", "(N)o" ))
+#try:
+#    if
+
+with open('config.toml', 'r') as conffile:
+    config = toml.loads(conffile.read())
+#print(config['DEFAULT']['folder'])
